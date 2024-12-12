b356decb488dd0e42351340d3dc7a1f99b703ea5
Bentavious <bentavious@TheHiddProject6.hsd1.ga.comcast.net>
2024-12-12 16:42:07 -0500

    first commit

---
+++ static/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded"), () =>{
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+    let form = document.getElementById("form")
+    form.addEventListener("submit)", (event) => {
+        event.preventDefault()
+        if(name&&email&&message){   
+            alert("Form succesfully submit")
+        }
+        else{
+            alert("form incomplete")
+        }
+
+    }
+ 
+}
